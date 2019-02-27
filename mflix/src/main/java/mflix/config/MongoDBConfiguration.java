7148bd8e12782d35987b8f760b30fc3c3465b815
Carlan Calazans <carlancalazans@gmail.com>
2019-02-27 21:10:13 +0000

    Ticket 15: Handling Timeouts
    - Updated app config class
    - Updated config file

--- mflix/src/main/java/mflix/config/MongoDBConfiguration.java
+++ mflix/src/main/java/mflix/config/MongoDBConfiguration.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +27,29 @@ public class MongoDBConfiguration {
 
     //TODO> Ticket: Handling Timeouts - configure the expected
     // WriteConcern `wtimeout` and `connectTimeoutMS` values
-    MongoClient mongoClient = MongoClients.create(connectionString);
+    final MongoClientSettings settings = MongoClientSettings
+    .builder()
+    .applicationName("mflix")
+    .applyConnectionString(connString)
+    .writeConcern(new WriteConcern("majority").withWTimeout(2500, TimeUnit.MILLISECONDS)) // wtimeoutMS
+    .applyToClusterSettings(builder -> 
+      builder
+        .applyConnectionString(connString)
+        .serverSelectionTimeout(5000, TimeUnit.MILLISECONDS) // mentioned on video
+        .build())
+    .applyToConnectionPoolSettings(builder ->
+      builder
+        .applyConnectionString(connString)
+        .maxSize(50)  // maxPoolSize
+        .build())
+    .applyToSocketSettings(builder ->
+      builder
+        .applyConnectionString(connString)
+        .connectTimeout(2000, TimeUnit.MILLISECONDS) // connectTimeoutMS
+        .build())
+    .build();
+    
+    MongoClient mongoClient = MongoClients.create(settings);
 
     return mongoClient;
   }
